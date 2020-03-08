ce51cf2cfdbeb6f13e6debe3e26badfb131127c8
QiuFeisheng <feishengqiu@163.com>
2020-03-09 00:58:03 +0800

    1

--- demo3-market/pages/cart/cart.js
+++ demo3-market/pages/cart/cart.js
@@ -5,62 +5,97 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    product: '',
+    productDataBase: [{
+        id: 0,
+        name: 'Mate00',
+        src: '/images/temp/cate10.jpg',
+        price: 1000
+      },
+      {
+        id: 1,
+        name: 'Mate10',
+        src: '/images/temp/cate11.jpg',
+        price: 1000
+      },
+      {
+        id: 2,
+        name: 'Mate20',
+        src: '/images/temp/cate12.jpg',
+        price: 1000
+      },
+      {
+        id: 3,
+        name: 'Mate30',
+        src: '/images/temp/cate13.jpg',
+        price: 1000
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    const pId = wx.getStorageSync('cartId')
+    let i = 0;
+    const productDataBase = this.data.productDataBase
+    for (i; i < productDataBase.length; i++) {
+      if (pId == productDataBase[i].id) {
+        this.setData({
+          product: productDataBase[i]
+        })
+      }
+    }
+    console.log(this.data.product)
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- demo3-market/pages/detail/detail.js
+++ demo3-market/pages/detail/detail.js
@@ -6,61 +6,76 @@ Page({
    */
   data: {
 
-  },
+    id: '',
 
+  },
+  addCart: function() {
+    wx.setStorageSync('cartId', this.data.id)
+    wx.showModal({
+      title: '添加到购物车成功',
+      content: '您的商品已经成功添加到购物车',
+      success() {
+        wx.switchTab({
+          url: '/pages/cart/cart',
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+    console.log(options.id)
+    this.setData({id:options.id})
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
